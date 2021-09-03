dd57637b28f0fffff02f9b0bc42c3045e9f30f47
hydrastro <postmaster@blackhost.xyz>
2021-09-03 13:05:15 +0200

    Fixing unused imports and var warnings

--- src/convert.rs
+++ src/convert.rs
@@ -1,5 +1,4 @@
 use std::time::UNIX_EPOCH;
-use std::process::Stdio;
 
 pub async fn convert_from_webm(buf: Vec<u8>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let time = std::time::SystemTime::now();
@@ -9,7 +8,7 @@ pub async fn convert_from_webm(buf: Vec<u8>) -> Result<Vec<u8>, Box<dyn std::err
     let input_filename = format!("{}.webm", nanos);
     let output_filename = format!("{}.mp4", nanos);
     tokio::fs::write(&input_filename, buf).await?;
-    let mut command = tokio::process::Command::new("ffmpeg")
+    tokio::process::Command::new("ffmpeg")
         .arg("-i")
         .arg(&input_filename)
         .arg("-vf")
